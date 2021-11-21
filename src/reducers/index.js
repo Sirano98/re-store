295055c8c0caeee124b34e3dd89bb85143d70ed0
Sirano98 <protsiuk1998@gmail.com>
2021-11-21 10:39:04 +0200

    add cart section

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,22 @@
 const initialState = {
   books: [],
   loading: true,
-  error: null
+  error: null,
+  cartItems: [
+    {
+      id: 1,
+      name: "Book 1",
+      count: 2,
+      price: 30
+    },
+    {
+      id: 2,
+      name: "Book 2",
+      count: 3,
+      price: 50
+    }
+  ],
+  orderTotal: 180
 };
 
 const reducer = (state = initialState, action) => {
@@ -10,6 +25,7 @@ const reducer = (state = initialState, action) => {
 
     case 'FETCH_BOOKS_REQUEST':
       return {
+        ...state,
         books: [],
         loading: true,
         error: null
@@ -17,6 +33,7 @@ const reducer = (state = initialState, action) => {
 
     case 'FETCH_BOOKS_SUCCESS':
       return {
+        ...state,
         books: action.payload,
         loading: false,
         error: null
@@ -24,6 +41,7 @@ const reducer = (state = initialState, action) => {
 
     case 'FETCH_BOOKS_FAILURE':
       return {
+        ...state,
         books: [],
         loading: false,
         error: action.payload
